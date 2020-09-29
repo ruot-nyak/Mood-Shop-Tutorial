bb6fb99bcdf8ff107a0acdca582a9bebbad79ed7
ruot-nyak <nyak.g.ruot@gmail.com>
2020-09-29 16:25:56 -0700

    add item and show item functions working

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,43 +1,55 @@
 const itemsContainer = document.getElementById("items");
 import data from "./data.js";
 
-
 for (let i = 0; i < data.length; ++i) {
-
   let newDiv = document.createElement("div");
   newDiv.className = "item";
 
   // create an image element
   let img = document.createElement("img");
- 
+
   img.src = data[i].image;
   img.width = 300;
   img.height = 300;
 
- 
   newDiv.appendChild(img);
 
   itemsContainer.appendChild(newDiv);
 
   let desc = document.createElement("p");
- 
+
   desc.innerText = data[i].desc;
-  
+
   newDiv.appendChild(desc);
 
   let price = document.createElement("P");
   price.innerText = data[i].price;
   newDiv.appendChild(price);
 
-  let button = document.createElement('button')
-  button.id = data[i].name
+  let button = document.createElement("button");
+  button.id = data[i].name;
 
+  button.dataset.price = data[i].price;
+  button.innerHTML = "Add to Cart";
+  newDiv.appendChild(button);
 
-  button.dataset.price = data[i].price
-  button.innerHTML = "Add to Cart"
-  newDiv.appendChild(button)
-  
-  itemsContainer.appendChild(newDiv)
-
-  
+  itemsContainer.appendChild(newDiv);
 }
+
+const cart = [ ]
+
+
+function addItem(name, price) {
+  const item = {name: name, price: price, qty: 1}
+  cart.push(item)
+
+}
+
+function showItems() {
+  console.log(`You have ${cart.length} items in your cart` )
+}
+
+addItem('happy', 5.99)
+addItem('sad', 5.99)
+
+showItems()
